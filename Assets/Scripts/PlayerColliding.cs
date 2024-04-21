ba6b6d0a87c810d3279f1d0ae6ffb00287851f4e
James <jtnguyencao@gmail.com>
2024-04-21 12:34:42 +0200

    add video + fix things (Zone1 only)

--- Assets/Scripts/PlayerColliding.cs
+++ Assets/Scripts/PlayerColliding.cs
@@ -11,8 +11,7 @@ public class PlayerColliding : MonoBehaviour
     private const string ExitTag = "Exit";
     private const string Exit1Tag = "Exit1";
     private const string Enter1Tag = "Enter1";
-    private const string Exit2Tag = "Exit2";
-    private const string Exit3Tag = "Exit3";
+    private const string EnterMontSaintMichelTag = "Mont";
     private const string Exit4Tag = "Exit4";
     private const string Chest1Tag = "Chest1";
     private const string Chest2Tag = "Chest2";
@@ -45,43 +44,43 @@ public class PlayerColliding : MonoBehaviour
     private bool FLisOn = false;
 
     private void Awake()
-        {
+    {
 
+    }
+
+    private void Start()
+    {
+        _gameManager = GameManager.Instance;
+    }
+
+    private void Update()
+    {
+        if (canPickUpGun && Input.GetKeyDown(KeyCode.E))
+        {
+            haveGun = true;
+            int weaponIndex = 0;
+            GameObject gun = GameObject.FindWithTag("Gun");
+            _gameManager.canvas.transform.GetChild(2).gameObject.SetActive(false);
+            Destroy(gun);
+            _gameManager.setActive(0);
+            canPickUpGun = false; // Reset flag
+        }
+        if (haveGun) {
+            _gameManager.canvas.transform.GetChild(2).gameObject.SetActive(false);
+            GameObject gun = GameObject.FindWithTag("Gun");
+            Destroy(gun);
         }
 
-        private void Start()
+        if (canPickUpFL && Input.GetKeyDown(KeyCode.E))
         {
-            _gameManager = GameManager.Instance;
+            int weaponIndex = 1;
+            GameObject fl = GameObject.FindWithTag("FL");
+            _gameManager.canvas.transform.GetChild(3).gameObject.SetActive(false);
+            Destroy(fl);
+            canPickUpFL = false;
+            _gameManager.setActive(1);
+            _gameManager.canvas.transform.GetChild(8).gameObject.SetActive(true);
         }
-
-        private void Update()
-        {
-            if (canPickUpGun && Input.GetKeyDown(KeyCode.E))
-            {
-                haveGun = true;
-                int weaponIndex = 0;
-                GameObject gun = GameObject.FindWithTag("Gun");
-                _gameManager.canvas.transform.GetChild(2).gameObject.SetActive(false);
-                Destroy(gun);
-                _gameManager.setActive(0);
-                canPickUpGun = false; // Reset flag
-            }
-            if (haveGun) {
-                _gameManager.canvas.transform.GetChild(2).gameObject.SetActive(false);
-                GameObject gun = GameObject.FindWithTag("Gun");
-                Destroy(gun);
-            }
-
-            if (canPickUpFL && Input.GetKeyDown(KeyCode.E))
-            {
-                int weaponIndex = 1;
-                GameObject fl = GameObject.FindWithTag("FL");
-                _gameManager.canvas.transform.GetChild(3).gameObject.SetActive(false);
-                Destroy(fl);
-                canPickUpFL = false;
-                _gameManager.setActive(1);
-                _gameManager.canvas.transform.GetChild(8).gameObject.SetActive(true);
-            }
 
 
 
@@ -161,24 +160,29 @@ public class PlayerColliding : MonoBehaviour
                 }
             }
 */
-        }
+    }
 
-        public void OnCollisionEnter(Collision collision)
+    public void OnCollisionEnter(Collision collision)
+    {
+        string url1 = "https://www.timographie360.fr/visites/visite-virtuelle/CMN/mont-saint-michel/?fbclid=IwAR2iypzYLU5_U4iuZESqf43Q1oWH47nn3OW0hdQ4TlJcadeTX2MWmJZcQVM";
+        if (collision.gameObject.CompareTag(ExitTag))
         {
-
-            if (collision.gameObject.CompareTag(ExitTag))
-                {
-                    _gameManager.Door1();
-                }
-
-            if (collision.gameObject.CompareTag(Enter1Tag))
-                {
-                    _gameManager.Enter1();
-                }
+            _gameManager.Door1();
         }
 
-        public void OnCollisionExit(Collision collision)
+        if (collision.gameObject.CompareTag(Enter1Tag))
         {
-        
+            _gameManager.Enter1();
         }
+
+        if (collision.gameObject.CompareTag(EnterMontSaintMichelTag))
+        {
+            Application.OpenURL(url1);
+        }
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+    
+    }
 }
--- Assets/Scripts/RaycastCamera.cs
+++ Assets/Scripts/RaycastCamera.cs
@@ -7,19 +7,20 @@ using UnityEngine.UI;
 public class RaycastCamera : MonoBehaviour
 {
     [SerializeField] private GameObject picture;
+    [SerializeField] private GameObject video;
     [SerializeField] private GameObject textObject;
+    private bool isPictureActive = false;
+    private bool isVideoActive = false;
     private bool isText = true;
     private Texture pictureTexture;
+    private const string EnterChartresTag = "Chartres";
 
     // Start is called before the first frame update
     void Start()
     {
         TextMeshProUGUI text = textObject.GetComponent<TextMeshProUGUI>();
-
-        if (picture.activeInHierarchy)
-        {
-            picture.SetActive(false);
-        }
+        picture.SetActive(false);
+        video.SetActive(false);
         if (isText)
         {
             text.SetText("");
@@ -31,33 +32,39 @@ public class RaycastCamera : MonoBehaviour
     void Update()
     {
         TextMeshProUGUI text = textObject.GetComponent<TextMeshProUGUI>();
-
-        if (Input.GetKeyUp(KeyCode.F) && picture.activeInHierarchy)
+        
+        if (Input.GetKeyUp(KeyCode.F))
         {
-            picture.SetActive(false);
+            if (isPictureActive)
+            {
+                picture.SetActive(false);
+                isPictureActive = false;
+            }
+            if (isVideoActive)
+            {
+                video.SetActive(false);
+                isVideoActive = false;
+            }
         }
-
         int layerMask = 1 << 7;
         // Does the ray intersect any objects excluding the player layer
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hit, 5, layerMask))
         {
-            if (!isText && !picture.activeInHierarchy)
+            if (!isText)
             {
-                text.SetText("Restez appuy� sur 'F' pour afficher l'image en grand �cran");
+                text.SetText("Restez appuyé sur 'F' pour afficher l'élément en grand écran");
                 isText = true;
             }
 
-            if (Input.GetKeyDown(KeyCode.F) && !picture.activeInHierarchy)
+            if (Input.GetKeyDown(KeyCode.F))
             {
-                picture.SetActive(true);
-                pictureTexture = GetTexture(hit.collider.gameObject);
-                SetTexture(pictureTexture);
-                text.SetText("");
+                StartCoroutine(ClearTextDelayed(text));
+                ToggleMedia(hit);
                 isText = false;
             }
             //Debug.Log("Did Hit");
         }
-        else
+         else
         {
             if (isText)
             {
@@ -68,17 +75,47 @@ public class RaycastCamera : MonoBehaviour
         }
     }
 
+    IEnumerator ClearTextDelayed(TextMeshProUGUI text)
+    {
+        yield return null; // Wait for the next frame
+        text.SetText(""); // Clear the text
+    }
+
+    void ToggleMedia(RaycastHit hit)
+    {
+        if (!isPictureActive)
+        {
+            if (hit.collider.CompareTag("Chartres"))
+            {
+                video.SetActive(true);
+                isVideoActive = true;
+            }
+            else
+            {
+                picture.SetActive(true);
+                isPictureActive = true;
+                pictureTexture = GetTexture(hit.collider.gameObject);
+                SetTexture(pictureTexture);
+            }
+        }
+        else if (!isVideoActive)
+        {
+            video.SetActive(true);
+            isPictureActive = false;
+            isVideoActive = true;
+        }
+    }
+
+
     Texture GetTexture(GameObject hit)
     {
         Material texture = hit.GetComponent<HG.DeferredDecals.Decal>().DecalMaterial;
-
         return texture.mainTexture;
     }
 
     void SetTexture(Texture texture)
     {
         float aspectRatio = (float)texture.width / (float)texture.height;
-        Debug.Log(texture.height);
         picture.GetComponent<AspectRatioFitter>().aspectRatio = aspectRatio;
         picture.GetComponent<RawImage>().texture = texture;
     }
